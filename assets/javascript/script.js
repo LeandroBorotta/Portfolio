b9ee42d80654bd0bea2fbf83140b03cbdd5a7c7a
LeandroBorotta <leandroborotta2006@gmail.com>
2023-02-05 00:32:09 -0300

    Fazendo a parte de Habilidades

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,6 +1,47 @@
 const fechar = document.querySelector('.fechar').addEventListener('click',fecharMenu);
 const menu = document.querySelector('.menu')
 const abrir = document.querySelector('.abrir').addEventListener('click', abrirMenu)
+const html = document.querySelector('.html').addEventListener('click', abrirContent)
+const css = document.querySelector('.css').addEventListener('click', abrirCss)
+const js = document.querySelector('.js').addEventListener('click', abrirJs)
+const git = document.querySelector('.gitHub').addEventListener('click', abrirGit)
+const conteudo = document.querySelector('.conteudo')
+const conteudoc = document.querySelector('.conteudoCss')
+const conteudoJs = document.querySelector('.conteudoJs')
+const conteudoGit = document.querySelector('.conteudoGit')
+
+function abrirGit(){
+    if(conteudoGit.classList.contains('active')){
+        conteudoGit.classList.remove('active')
+    } else{
+        conteudoGit.classList.add('active')
+    }
+}
+
+function abrirJs(){
+    if(conteudoJs.classList.contains('active')){
+        conteudoJs.classList.remove('active')
+    } else{
+        conteudoJs.classList.add('active')
+    }
+}
+
+function abrirCss(){
+    if(conteudoc.classList.contains('active')){
+        conteudoc.classList.remove('active')
+    } else{
+        conteudoc.classList.add('active')
+    }
+}
+
+
+function abrirContent() {
+    if(conteudo.classList.contains('active')){
+        conteudo.classList.remove('active')
+    } else{
+        conteudo.classList.add('active')
+    }
+} 
 
 function abrirMenu() {
     menu.classList.add('active')
